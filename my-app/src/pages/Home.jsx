af4f1a3dbcf8ba624f07f467ded94b86f40dc1b2
Sajin Saji George <sajinsajigeorge@gmail.com>
2025-02-07 11:10:11 +0530

    Initial Vite React setup

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+    return (
+        <div>
+        <h1>Sajin Saji George</h1>
+        <h3>Contact: <a href="mailto:youremail@example.com">sajin.sajigeorge.s65@kalvium.community</a></h3>
+        </div>
+    );
+}
+
+export default Home;
